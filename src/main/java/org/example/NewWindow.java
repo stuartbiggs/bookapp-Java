70a8a05669144228f7943b762f1467583fb60efb
stuartbiggs <stuartbiggs1973@gmail.com>
2023-09-06 18:42:52 +0100

    label added

--- src/main/java/org/example/NewWindow.java
+++ src/main/java/org/example/NewWindow.java
@@ -2,15 +2,11 @@
 package org.example;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class NewWindow extends JFrame {
 
     JFrame frame = new JFrame();
-    JLabel label = new JLabel("Hello");
+    //JLabel label = new JLabel("Hello");
     JButton button = new JButton("Exit");
     NewWindow(){
 
@@ -20,10 +16,10 @@ public class NewWindow extends JFrame {
             frame.dispose();
         });
 
-        label.setBounds(0,0,100, 50);
+        //label.setBounds(0,0,100, 50);
 
         frame.add(button);
-        frame.add(label);
+        //frame.add(label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setLayout(null);
